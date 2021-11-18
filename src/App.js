a1c490866db9203e6e6c4abd8f1ca7b20b8a954b
iamabhas <abhas123mnm@gmail.com>
2021-11-18 14:44:21 +0545

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import { useState, useEffect } from "react";
+import "./styles.css";
+import Movies from "./Movies";
+const url = "https://ghibli-movie-watcher-api.herokuapp.com/ghibli_movies";
+export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const getMovies = async () => {
+    setLoading(true);
+    try {
+      const respose = await fetch(url);
+      const movies = await respose.json();
+      setLoading(false);
+      setMovies(movies);
+    } catch (error) {
+      setLoading(false);
+      alert(error);
+    }
+  };
+  const removeMovie = (id) => {
+    let newMovies = movies.filter((movie) => {
+      return movie.id != id;
+    });
+    setMovies(newMovies);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  if (loading) {
+    return <h1 className="loading">Loading...</h1>;
+  }
+  if (movies.length === 0) {
+    return (
+      <main className="refresh-page">
+        <p className="alert">
+          {" "}
+          Opps ! Looks like you're not interested in any movies here
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button className="btn restore-btn" onClick={getMovies}>
+          Restore Movies
+        </button>
+      </main>
+    );
+  }
+  return <Movies movies={movies} removeMovie={removeMovie} />;
 }
-
-export default App;
